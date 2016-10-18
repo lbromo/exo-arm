1629404af41a3dfdb3821c21dae1c5a9a026e248
Lasse Bromose <lbromo@protonmail.ch>
2016-10-18 21:01:35 +0200

    Simulating --- awesome awesome

--- model/exo_dynamic_model.m
+++ model/exo_dynamic_model.m
@@ -1,21 +1,23 @@
-function [xdot] = exo_dynamic_model(x,torque, params)
+function [xdot] = exo_dynamic_model(t, x,torque, params)
 
-  theta = x(:,1:2);
-  thetadot = x(:,2:4)
+  x;
+
+  theta = x(1:2);
+  thetadot = x(3:4);
 
 %
 % initheta=[0 0];
 % inithetad=[0 0];
 % inithetadd=[0 0];
 
-  V=v_matrix(theta,thetadot, params.l2, params.m2, params.a1, params.a2)
-  Minv=minv_matrix(theta, params.l1, params.l2, params.m1, params.m2, params.a1, params.a2, params.I1, params.I2)
-  G=g_matrix(theta, params.g, params.l1, params.l2, params.m1, params.m2, params.a1 ,params.a2)
-  F=f_matrix(thetadot, params.vm, params.cm)
+  V=v_matrix(theta,thetadot, params.l2, params.m2, params.a1, params.a2);
+  Minv=minv_matrix(theta, params.l1, params.l2, params.m1, params.m2, params.a1, params.a2, params.I1, params.I2);
+  G=g_matrix(theta, params.g, params.l1, params.l2, params.m1, params.m2, params.a1 ,params.a2);
+  F=f_matrix(thetadot, params.vm, params.cm);
 
 
   thetadotdot = Minv*(torque-V-G-F);
 
-  xdot = [thetadot; thetadotdot]
+  xdot = [thetadot; thetadotdot];
 
 end
--- model/exo_main.m
+++ model/exo_main.m
@@ -1,13 +1,20 @@
 ParametersScript
 
-torque = [0.0004; 0]
+x0 = [
+      0;
+      0;
+      0;
+      0;
+];
 
-theta = [0;0]
-thetadot = [0;0]
-thetadotdot = [0;0]
+u = [
+       0;
+       0.4
+];
 
-for i = 1:10
-thetadotdot(:,end+1) = exo_dynamic_model(theta(:,end), thetadot(:,end),torque, params);
-thetadot(:,end+1) = trapz(thetadotdot(:,end)');
-theta(:,end+1) = trapz(thetadot(:,end)');
-end
+t_span = [
+          1;
+          3
+];
+
+[t,x] = ode45(@exo_dynamic_model, t_span, x0, [], u, params);
