a84eea7fdb5f5afb77d1881c46ceca0f24090ca5
Lasse Bromose <lbromo@protonmail.ch>
2016-10-20 13:49:36 +0200

    Now simulating

--- model/exo_dynamic_model.m
+++ model/exo_dynamic_model.m
@@ -1,20 +1,28 @@
-function [xdot] = exo_dynamic_model(t, x,torque, params)
+function [xdot] = exo_dynamic_model(t, x, torque, params)
+  global g_V g_G g_F;
+
   theta = x(1:2);
   thetadot = x(3:4);
 
-  params.arm.plot(theta');
+  if t > 0.1
+    torque = [0; 0];
+  end
+
+  t
+
+  %params.arm.plot(theta');
 %
 % initheta=[0 0];
 % inithetad=[0 0];
 % inithetadd=[0 0];
 
-  V=v_matrix(theta,thetadot, params.l2, params.m2, params.a1, params.a2);
   Minv=minv_matrix(theta, params.l1, params.l2, params.m1, params.m2, params.a1, params.a2, params.I1, params.I2);
+
+  V=v_matrix(theta,thetadot, params.l2, params.m2, params.a1, params.a2);
   G=g_matrix(theta, params.g, params.l1, params.l2, params.m1, params.m2, params.a1 ,params.a2);
   F=f_matrix(thetadot, params.vm, params.cm);
 
-
-  thetadotdot = Minv*(torque-V-G-F);
+  thetadotdot = Minv*(torque-(V+G+F));
 
   xdot = [thetadot; thetadotdot];
 
--- model/exo_main.m
+++ model/exo_main.m
@@ -1,20 +1,29 @@
 ParametersScript
 
 x0 = [
-      0;
+      0
       0;
       0;
       0;
 ];
 
 u = [
-       0;
-       4
+       4;
+       0
 ];
 
-t_span = [
-          1;
-          3
-];
+t_span = [0 2];
 
-[t,x] = ode45(@exo_dynamic_model, t_span, x0, [], u, params);
+[t,x] = ode113(@exo_dynamic_model, t_span, x0, [], u, params);
+
+
+%% So we have way to many samples, and no "specific" sampling time..
+%% We "create" one by taking unique time samples with a 0.01 spacing
+[C,ia,ic] = uniquetol(t, 0.01);
+
+theta1 = x(ia,1);
+theta2 = x(ia,2);
+
+theta = [theta1 theta2];
+
+params.arm.plot(theta, 'fps', length(theta)/t_span(end), 'loop');
