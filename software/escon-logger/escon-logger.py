c3d91c93bebde7791912491fb6735e4106033e44
DisasterMaster <mortenrechter@hotmail.com>
2016-10-21 12:26:08 +0200

    escon logger

---
+++ software/escon-logger/escon-logger.py
@@ -0,0 +1,11 @@
+import serial
+
+
+if __name__ == "__main__":
+	ser = serial.Serial()
+	ser.port = /dev/ttyMCC/
+	ser.baudrate = 9600
+	ser.open()
+	if ser.isOpen():
+		while True:
+			print ser.readline()
