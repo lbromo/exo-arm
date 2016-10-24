2991a11d188dcb0429468f330eb49460e83a1e01
Lasse Bromose <lbromo@protonmail.ch>
2016-10-24 13:28:56 +0200

    Discrete time model

---
+++ model/f.m
@@ -0,0 +1,14 @@
+function [xdot] = f(x, u, params)
+  theta = x(1:2,end);
+  thetadot = x(3:4,end);
+
+  Minv=minv_matrix(theta, params.l1, params.l2, params.m1, params.m2, params.a1, params.a2, params.I1, params.I2);
+
+  V=v_matrix(theta,thetadot, params.l2, params.m2, params.a1, params.a2);
+  G=g_matrix(theta, params.g, params.l1, params.l2, params.m1, params.m2, params.a1 ,params.a2);
+  F=f_matrix(thetadot, params.vm, params.cm);
+
+  thetadotdot = Minv*(u-(V+G+F));
+
+  xdot = [thetadot; thetadotdot];
+end
