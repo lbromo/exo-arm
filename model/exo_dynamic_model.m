15f1e90b73bf462a38c6b0614fd97fa683760d98
Lasse Bromose <lbromo@protonmail.ch>
2016-10-24 13:29:10 +0200

    Minor cleanup

--- model/exo_dynamic_model.m
+++ model/exo_dynamic_model.m
@@ -1,18 +1,10 @@
 function [xdot] = exo_dynamic_model(t, x, torque, params)
-  global g_V g_G g_F;
-
   theta = x(1:2);
   thetadot = x(3:4);
 
   if t > 5
     torque = [0; 0];
   end
-
-  %params.arm.plot(theta');
-%
-% initheta=[0 0];
-% inithetad=[0 0];
-% inithetadd=[0 0];
 
   Minv=minv_matrix(theta, params.l1, params.l2, params.m1, params.m2, params.a1, params.a2, params.I1, params.I2);
 
