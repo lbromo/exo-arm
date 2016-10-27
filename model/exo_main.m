03f4f2b955c5ba8fc5fbc45f5fe08ff9879d9da7
DisasterMaster <mortenrechter@hotmail.com>
2016-10-27 09:54:12 +0200

    uncommented stuff until exo-main worked, added coloumb friction

--- model/exo_main.m
+++ model/exo_main.m
@@ -14,17 +14,17 @@ u = [
 
 t_span = [0 10];
 
-[t,x] = ode113(@exo_dynamic_model, t_span, x0, [], u, params);
+% [t,x] = ode113(@exo_dynamic_model, t_span, x0, [], u, params);
 
 
 %% So we have way to many samples, and no "specific" sampling time..
 %% We "create" one by taking unique time samples with a 0.01 spacing
-%[C,ia,ic] = uniquetol(t, 0.001);
+% [C,ia,ic] = uniquetol(t, 0.001);
 
-theta1 = x(ia,1);
-theta2 = x(ia,2);
+%theta1 = x(ia,1);
+%theta2 = x(ia,2);
 
-theta = [theta1 theta2];
+%theta = [theta1 theta2];
 
 %params.arm.plot(theta, 'fps', length(theta)/t_span(end), 'loop');
 
@@ -36,9 +36,9 @@ xd = [0;0;0;0];
 u = [1; 0.5];
 
 for k = 1:Tend/Ts
-  %if(mod(k,10) == 0)
-  %  params.arm.plot(xd(1:2,k)', 'fps', 1/(Ts*10))
-  %end
+  if(mod(k,10) == 0)
+    params.arm.plot(xd(1:2,k)', 'fps', 1/(Ts*10))
+  end
 
   if (k > 1/Ts)
     u = [0; 0];
