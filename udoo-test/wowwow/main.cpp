8d353bbb10b85ae71aeaa524d59789039fa6498e
Lasse Bromose <lbromo@protonmail.ch>
2016-10-30 14:56:32 +0100

    Moved FreeRTOS - can cross compile

---
+++ udoo-test/wowwow/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <wiring_digital.h>
+#include "libraries/FreeRTOSv9.0.0/FreeRTOS/Source/include/FreeRTOS.h"
+#include "libraries/FreeRTOSv9.0.0/FreeRTOS/Source/include/task.h"
+/*
+#include "libraries/FreeRTOSv9.0.0/FreeRTOS/Source/include/semphr.h"
+#include "libraries/FreeRTOSv9.0.0/FreeRTOS/Source/include/queue.h"
+#include "libraries/FreeRTOSv9.0.0/FreeRTOS/Source/include/timers.h"
+#include "libraries/FreeRTOSv9.0.0/FreeRTOS/Source/include/event_groups.h"
+#include "libraries/FreeRTOSv9.0.0/FreeRTOS/Source/include/mpu_prototypes.h"
+*/
+
+
+
+void vBlinkTask(void *pvParameters){
+  bool flag = false;
+  if(xTaskCreate){}
+  while(true){
+    digitalWrite(13, flag);
+    flag = !flag;
+    vTaskDelay(500/portTICK_RATE_MS);
+  }
+}
+
+
+int main(){
+  pinMode(13, OUTPUT);
+
+  // put your setup code here, to run once:
+  xTaskCreate(vBlinkTask, "Blinker", 1000, NULL, 0, NULL);
+  vTaskStartScheduler();
+}
