b69837d24da91ddff8495f1da25b840edb9b75cd
Lasse Bromose <lbromo@protonmail.ch>
2016-11-03 13:48:36 +0100

    dont use addr

--- software/escon-logger/escon-logger.ino
+++ software/escon-logger/escon-logger.ino
@@ -38,131 +38,131 @@ Scheduler runner;
 
 int getPos(int joint){
 
-	if (joint == SHOULDER){
-		return analogRead(pin_pos1);
-	}
-	else if (joint == ELBOW){
-		return analogRead(pin_pos2);
-	}
-	else{
-		return -1;
-	}
+        if (joint == SHOULDER){
+                return analogRead(pin_pos1);
+        }
+        else if (joint == ELBOW){
+                return analogRead(pin_pos2);
+        }
+        else{
+                return -1;
+        }
 }
 
 int getCur(int joint){
 
-	if (joint == SHOULDER){
-		return analogRead(pin_cur1);
-	}
-	else if (joint == ELBOW){
-		return analogRead(pin_cur2);
-	}
-	else{
-		return -1;
-	}
+        if (joint == SHOULDER){
+                return analogRead(pin_cur1);
+        }
+        else if (joint == ELBOW){
+                return analogRead(pin_cur2);
+        }
+        else{
+                return -1;
+        }
 }
 
 int getVel(int joint){
 
-	if (joint == SHOULDER){
-		return analogRead(pin_vel1);
-	}
-	else if (joint == ELBOW){
-		return analogRead(pin_vel2);
-	}
-	else{
-		return -1;
-	}
+        if (joint == SHOULDER){
+                return analogRead(pin_vel1);
+        }
+        else if (joint == ELBOW){
+                return analogRead(pin_vel2);
+        }
+        else{
+                return -1;
+        }
 }
 
 void sendMeas(int joint, unsigned long time, int ang, int vel, int cur){
 
-Serial.println(START_CHAR);
-Serial.print(',');
-Serial.print(joint);
-Serial.print(',');
-Serial.print(time);
-Serial.print(',');
-Serial.print(ang);
-Serial.print(',');
-Serial.print(vel);
-Serial.print(',');
-Serial.println(cur);
+        Serial.println(START_CHAR);
+        Serial.print(',');
+        Serial.print(joint);
+        Serial.print(',');
+        Serial.print(time);
+        Serial.print(',');
+        Serial.print(ang);
+        Serial.print(',');
+        Serial.print(vel);
+        Serial.print(',');
+        Serial.println(cur);
 
 }
 
 void measure(){
-int vel1, cur1, ang1;
-int vel2, cur2, ang2;
-unsigned long time;
+        int vel1, cur1, ang1;
+        int vel2, cur2, ang2;
+        unsigned long time;
 
-ang1 = getPos(SHOULDER);
-cur1 = getCur(SHOULDER);
-vel1 = getVel(SHOULDER);
-time = millis();
-sendMeas(SHOULDER, time, ang1, vel1, cur1);
+        ang1 = getPos(SHOULDER);
+        cur1 = getCur(SHOULDER);
+        vel1 = getVel(SHOULDER);
+        time = millis();
+        sendMeas(SHOULDER, time, ang1, vel1, cur1);
 
-ang2 = getPos(ELBOW);
-cur2 = getCur(ELBOW);
-vel2 = getVel(ELBOW);
-sendMeas(ELBOW, time, ang2, vel2, cur2);
+        ang2 = getPos(ELBOW);
+        cur2 = getCur(ELBOW);
+        vel2 = getVel(ELBOW);
+        sendMeas(ELBOW, time, ang2, vel2, cur2);
 }
 
 
 void set_pwm(){
-int Bcount, outdir1, outpwm1, outdir2, outpwm2;
-Bcount=Serial.available();
-char input_Buffer[10];
-char pwm1_buffer[3];
-char pwm2_buffer[3];
+        int Bcount, outdir1, outpwm1, outdir2, outpwm2;
+        Bcount=Serial.available();
+        char input_Buffer[10];
+        char pwm1_buffer[3];
+        char pwm2_buffer[3];
 
-if (Bcount >= 9){ /* 1 byte for direction (0 or 1), 3 byte for pwm (0-100 %), 1 byte for newline*/
-Serial.readBytes(input_Buffer, Bcount);
+        if (Bcount >= 9){ /* 1 byte for direction (0 or 1), 3 byte for pwm (0-100 %), 1 byte for newline*/
+                Serial.readBytes(input_Buffer, Bcount);
 
-outdir1=atoi(&input_Buffer[0]);
-memcpy(pwm1_buffer, &input_Buffer[1], 3);
-outpwm1=atoi(pwm1_buffer);
+                outdir1=atoi(input_Buffer[0]);
+                memcpy(pwm1_buffer, &input_Buffer[1], 3);
+                outpwm1=atoi(pwm1_buffer);
 
-outdir2=atoi(&input_Buffer[4]);
-memcpy(pwm2_buffer, &input_Buffer[5], 3);
-outpwm2=atoi(pwm2_buffer);
+                outdir2=atoi(input_Buffer[4]);
+                memcpy(pwm2_buffer, &input_Buffer[5], 3);
+                outpwm2=atoi(pwm2_buffer);
 
-digitalWrite(pin_dir1,outdir1);
-digitalWrite(pin_dir2,outdir2);
-analogWrite(pin_pwm1,outpwm1);
-analogWrite(pin_pwm2,outpwm2);
-digitalWrite(pin_on1,1);
-digitalWrite(pin_on2,1);
-}
+                digitalWrite(pin_dir1,outdir1);
+                digitalWrite(pin_dir2,outdir2);
+                analogWrite(pin_pwm1,outpwm1);
+                analogWrite(pin_pwm2,outpwm2);
+                digitalWrite(pin_on1,1);
+                digitalWrite(pin_on2,1);
+        }
 
 }
 
 
 void setup(){
-    Serial.begin(9600);
+        Serial.begin(9600);
 
-  Serial.println("Scheduler running");
-  
-  runner.init();
-  
-  runner.addTask(t1);
-  
-  runner.addTask(t2);
-  
-  t1.enable();
- 
-  t2.enable();
+        Serial.println("Scheduler running");
 
- pinMode(pin_on1,OUTPUT);
- pinMode(pin_on2,OUTPUT);
- pinMode(pin_dir1,OUTPUT);
- pinMode(pin_dir2,OUTPUT); 
+        runner.init();
+
+        runner.addTask(t1);
+
+        runner.addTask(t2);
+
+        t1.enable();
+
+        t2.enable();
+
+        pinMode(pin_on1,OUTPUT);
+        pinMode(pin_on2,OUTPUT);
+        pinMode(pin_dir1,OUTPUT);
+        pinMode(pin_dir2,OUTPUT);
 }
 
 
 
 void loop(){
 
-runner.execute();
+        runner.execute();
 
 }
