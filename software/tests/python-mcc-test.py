179fb962794615ca51d09a6e107a12cd78dab809
DisasterMaster <mortenrechter@hotmail.com>
2016-11-04 15:02:34 +0100

    tests

--- software/tests/python-mcc-test.py
+++ software/tests/python-mcc-test.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 	ser = serial.Serial(timeout = TIMEOUT, baudrate = BAUD, port = SER_PORT)
 
 	if not ser.isOpen():
+		print("Opening Serial...")
 		ser.open()
 		while ser.inWaiting > 0:
 			ser.read()
