088cf2279c588ec43a7aac6552e302c138b1eb00
DisasterMaster <mortenrechter@hotmail.com>
2016-11-10 13:08:35 +0100

    additions to parest scripst

--- software/seq_parest/seq_parest.py
+++ software/seq_parest/seq_parest.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 import sys
 
-PULSE_PERIOD_S = 5
+PULSE_PERIOD_S = 1
 T_END_S = 10
 SAMPLE_F_HZ = 100
 SAMPLE_PERIOD_S = 1/SAMPLE_F_HZ
@@ -13,9 +13,11 @@ SAMPLE_PERIOD_S = 1/SAMPLE_F_HZ
 SER_PORT = sys.argv[1]
 BAUD = 115200
 
-INPUT_FILE = "input.log"
-MOTOR1_FILE = "motor1.log"
-MOTOR2_FILE = "motor2.log"
+ts = str(time.time())
+
+INPUT_FILE = "input" + ts + ".log"
+MOTOR1_FILE = "motor1" + ts + ".log"
+MOTOR2_FILE = "motor2" + ts + ".log"
 START = str('b\'$\\r\\n\'')
 MOTORIDINDEX = 3
 MSGSTARTINDEX = 5
@@ -59,29 +61,33 @@ if __name__ == "__main__":
     input_file_h.write("on1,dir1,pwm1,on2,dir2,pwm2\n")
     t = np.linspace(0,T_END_S,T_END_S*SAMPLE_F_HZ)
     
-    sig_motor1 = signal.square(1/PULSE_PERIOD_S*2*np.pi*t, duty=0.5)
+    sig_motor1 = 30 * signal.square(1/PULSE_PERIOD_S*2*np.pi*t, duty=0.5)
     sig_motor2 = np.zeros(len(t))
 
-    for i in range(0,len(t)):
-        sig_motor1[i] = clamp(sig_motor1[i],0,255)
-        sig_motor2[i] = clamp(sig_motor2[i],0,255)
-
     on1 = 1
-    dir1 = 1
+    dir1 = signal.square(1/PULSE_PERIOD_S*2*np.pi*t, duty=0.5)
     on2 = 1
     dir2 = 1
 
+    for i in range(0,len(t)):
+        sig_motor1[i] = clamp(sig_motor1[i],30,230)
+        sig_motor2[i] = clamp(sig_motor2[i],30,230)
+        dir1[i] = clamp(dir1[i], 0,1);
+
+
+
+
     try:
         if ser.isOpen():
-            for i in range(0, len(t)):
+            for i in range(0, len(t)-1):
                 starttime = time.time()
                 
                 # Parsing
-                a = '$' + str(on1) + str(dir1) + str(int(sig_motor1[i])).zfill(3) + str(on2) + str(dir2) + str(int(sig_motor2[i])).zfill(3)
+                a = '$' + str(on1) + str(int(dir1[i])) + str(int(sig_motor1[i])).zfill(3) + str(on2) + str(dir2) + str(int(sig_motor2[i])).zfill(3)
                 out = str.encode(a)
                 ser.write(out)
 
-                input_file_h.write(str(on1) + ',' + str(dir1) + ',' + str(int(sig_motor1[i])).zfill(3) + ',' + str(on2) + ',' + str(dir2) + ',' + str(int(sig_motor2[i])).zfill(3) + '\n')
+                input_file_h.write(str(on1) + ',' + str(dir1[i]) + ',' + str(int(sig_motor1[i])).zfill(3) + ',' + str(on2) + ',' + str(dir2) + ',' + str(int(sig_motor2[i])).zfill(3) + '\n')
                 print("Ctrl:" + str(out))
                 log1msg(ser)
                 log1msg(ser)
@@ -90,6 +96,7 @@ if __name__ == "__main__":
                 print(time_to_sleep)
                 if time_to_sleep > 0:
                     time.sleep(time_to_sleep)
+            ser.write(b'$0001000010')
 
     except Exception as e:
         print(e)
