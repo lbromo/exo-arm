9e66aac38ae3b9e28a71aa3a99479bb6dc2ee494
Lasse Bromose <lbromo@protonmail.ch>
2016-11-22 08:45:55 +0100

    plot to 140 degrees

--- software/pythonmodel/muscle.py
+++ software/pythonmodel/muscle.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
 
   length = []
 
-  for a in range(0,180):
+  for a in range(0,140):
     angs = [0,0,a,0]
     length.append(m.__get_muscle_length__(angs))
     F.append(m.get_force_estimate(angs, activation_level))
