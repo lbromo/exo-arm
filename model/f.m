2b95658585dd7cb0807a7dc1535d5743bfff600c
Bjarke <bjarkenroepoulsen@gmail.com>
2017-02-07 15:52:57 +0100

    mech arm der nu virker i matlab- Wuhuuu

--- model/f.m
+++ model/f.m
@@ -2,10 +2,10 @@ function [xdot] = f(x, u, params)
   theta = x(1:2,end);
   thetadot = x(3:4,end);
 
-  Minv=minv_matrix(theta, params.l1, params.l2, params.m1, params.m2, params.a1, params.a2, params.I1, params.I2);
+  Minv=minv_matrix(theta, params.l1, params.l2, params.m1, params.m2, params.a1, params.a2, params.I1, params.I2, params.In1, params.In2, params.N);
   V=v_matrix(theta,thetadot, params.l2, params.m2, params.a1, params.a2);
   G=g_matrix(theta, params.g, params.l1, params.l2, params.m1, params.m2, params.a1 ,params.a2);
-  F= f_matrix(thetadot, params.vm, params.cm);
+  F=f_matrix(thetadot, params.vm, params.cm, params.N);
 
 % if (abs(F) > abs(u-V-G))
 % 	if (abs(thetadot) < 0.01)
