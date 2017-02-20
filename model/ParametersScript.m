4c9ffccbd76dfbed196894941250742cb9e05843
Bjarke <bjarkenroepoulsen@gmail.com>
2017-02-20 09:33:42 +0100

    matlab model

--- model/ParametersScript.m
+++ model/ParametersScript.m
@@ -13,38 +13,39 @@ disp('Parametersscript')
 %% Constants
 g   = 9.81;     % [m/s^2] Gravitational acceleration
 Ts	= 0.004;	% [s] Sampling period
-
+kt1  = 0.1;     % motor constant shoulder/m1
+kt2  = 0.1;     % motor constant elbow/m2
 %% Constants - Link 1:
 d1  = 0;        % [m] Distance from {0} to {1} along z0
 a1  = 330e-3;        % [m] Distance from {0} to {1} along x0
 alpha1 = 0;  % [rad] Rotation of z1 compared to z0
-l1  = 224e-3;   % [m] Distance from {1} to CoM of link 1
-m1  = 1491.070e-3;	% [kg] Mass of link 1
+l1  = 244.9300e-3;   % [m] Distance from {1} to CoM of link 1
+m1  = 1412.26e-3;	% [kg] Mass of link 1
 
 %% Constants - Link 2:
 d2  = 0;        % [m] Distance from {1} to {2} along z1
 a2  = 227e-3;    % [m] Distance from {1} to {2} along x1
 alpha2 = 0;     % [rad] Rotation of z2 compared to z1
-l2  = 89e-3;   % [m] Distance from {2} to CoM of link 2
-m2  = 547.77e-3;	% [kg] Mass of link 2
+l2  = 174.2420e-3;   % [m] Distance from {2} to CoM of link 2
+m2  = 307.424e-3;	% [kg] Mass of link 2
 
 
 %% Inertias
 %[I1, I2] = InertiaCalculations([a1 a2],[m1 m2]);
 
-I1_xx = 17401661.51e-9
-I1_yy = 49708861.39e-9
-I1_zz = 33589941.15e-9
-I1_xy = -9363.03e-9
-I1_yz = -40923.12e-9
-I1_xz = -10657124.30e-9
+I1_xx = 1237092.59e-9
+I1_yy = 21058153.33e-9
+I1_zz = 20909842.62e-9
+I1_xy = -5512.53e-9
+I1_yz =  584.44e-9
+I1_xz = 2358470.81e-9
 
-I2_xx = 658642.154e-9
-I2_yy = 3806398.210e-9
-I2_zz = 3444384.466e-9
-I2_xy = 3462.100e-9
-I2_yz = -4984.582e-9
-I2_xz = -443444.240e-9
+I2_xx = 81789.271e-9
+I2_yy = 1069200.617e-9
+I2_zz = 1118883.857e-9
+I2_xy = 0.001e-9
+I2_yz = 0
+I2_xz = -26501.404e-9
 
 I1 = [I1_xx, I1_xy, I1_yz
       I1_xy, I1_yy, I1_yz
@@ -57,8 +58,8 @@ I2 = [I2_xx, I2_xy, I2_yz
 Izz1=I1(3,3);Izz2=I2(3,3);
 
 %% Motor Inertia
-In1 = 0.282e-4 + 1210e-7; % shoudler gear annd motor inertia
-In2 = 0.282e-4 + 181e-7;  % elbow gear annd motor inertia
+In1 = 0.282e-4 + 1210e-7; % shoudler gear and motor inertia
+In2 = 0.282e-4 + 181e-7;  % elbow gear and motor inertia
 
 %% Gear Ratio
 
@@ -90,7 +91,8 @@ params.vm = vm;
 params.In1 = In1;
 params.In2 = In2;
 params.N = N;
-
+params.kt1 = kt1;
+params.kt2 = kt2;
 %% Robotics Toolbox Implementation
 %L(1) = Link('d', d1,	'a', a1,   'alpha', alpha1);
 %L(2) = Link('d', d2,	'a', a2,   'alpha', alpha2);
