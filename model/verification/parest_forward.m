dbd8be0e4f44d32c2219f00a3802c5cfa23da6f3
Bjarke <bjarkenroepoulsen@gmail.com>
2017-03-06 11:24:16 +0100

    model

--- model/verification/parest_forward.m
+++ model/verification/parest_forward.m
@@ -3,10 +3,10 @@ function [xd] = parest_forward(c_est, u)
 %   Detailed explanation goes here
 global Ts params x0;
 tmp_pars = params;
-tmp_pars.cm = c_est(1:2);
- tmp_pars.vm=c_est(3:4);
-tmp_pars.sigmoidpar=c_est(5:6);
- tmp_pars.hast=c_est(7:8);
+%tmp_pars.cm = c_est(1:2);
+%tmp_pars.vm=c_est(3:4);
+%tmp_pars.sigmoidpar=c_est(5:6);
+ tmp_pars.hast=c_est(1:2);
 xd(:,1)=x0;
 for k = 1:length(Ts)
 %    disp(size(f(xd(:,k), u(:,k), tmp_pars, 0)))
