c9563ac58046719fb3ead1d73a20f32a3e97d9e8
Lasse Bromose <lbromo@protonmail.ch>
2017-03-23 09:29:17 +0100

    cd into parest dir

--- run_parest.sh
+++ run_parest.sh
@@ -6,7 +6,8 @@ do
             source venv/bin/activate;
             export LD_LIBRARY_PATH=/afs/ies.auc.dk/group/17gr1035/Private/pagmo/build/lib;
             export PYTHONPATH=/afs/ies.auc.dk/group/17gr1035/Private/pagmo/build/usr/lib/python2.7/dist-packages;
-            python software/model/muscles/parameter_estimation/parest.py parest$i.pickle;
+            cd software/model/muscles/parameter_estimation
+            python parest.py parest$i.pickle;
             exec bash
            "
 done
