52e04ca2633b0528fc8db10ca1f87255fac17cf6
Lasse Bromose <lbromo@protonmail.ch>
2017-03-23 09:38:19 +0100

    awesome

--- run_parest.sh
+++ run_parest.sh
@@ -1,13 +1,18 @@
-for i in {1..4}
+#!/bin/bash
+for j in {1..4}
 do
-    screen -dmS "parest$i" \
-           bash -c         \
-           "
-            source venv/bin/activate;
-            export LD_LIBRARY_PATH=/afs/ies.auc.dk/group/17gr1035/Private/pagmo/build/lib;
-            export PYTHONPATH=/afs/ies.auc.dk/group/17gr1035/Private/pagmo/build/usr/lib/python2.7/dist-packages;
-            cd software/model/muscles/parameter_estimation
-            python parest.py parest$i.pickle;
-            exec bash
-           "
+    for i in {1..16}
+    do
+        screen -dmS "parest_$j_$i" \
+               bash -c         \
+               "
+               source venv/bin/activate;
+               export LD_LIBRARY_PATH=/afs/ies.auc.dk/group/17gr1035/Private/pagmo/build/lib;
+               export PYTHONPATH=/afs/ies.auc.dk/group/17gr1035/Private/pagmo/build/usr/lib/python2.7/dist-packages;
+               cd software/model/muscles/parameter_estimation
+               python parest.py parest_$j_$i.pickle;
+               exec bash
+               "
+    done
+    sleep 3600
 done
