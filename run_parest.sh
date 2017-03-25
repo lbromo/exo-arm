647c02e005af932fa4c52a6bc1f4814823bb0ab5
Lasse Bromose <lbromo@protonmail.ch>
2017-03-25 14:04:17 +0100

    run stuff

--- run_parest.sh
+++ run_parest.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-    CORES=1
+    RUNS=1
 else
-    CORES=$1
+    RUNS=$1
 fi
 
 echo "$CORES"
 
-for i in {1..$CORES}
+for i in {1..$RUNS}
 do
-    screen -dmS "parest_$i" \
-           bash -c         \
-           "
-           source venv/bin/activate;
-           export LD_LIBRARY_PATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/lib;
-           export PYTHONPATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/lib/python2.7/dist-packages;
-           cd software/model/muscles/parameter_estimation;
-           nice -n 10 python parest.py parest_$i.pickle;
-           exec bash
-           "
+    if [ $(ps -ef | grep -v grep | grep scrape_data.php | wc -l) -lt  10]
+    then
+        screen -dmS "parest_$i" \
+               bash -c         \
+               "
+               source venv/bin/activate;
+               export LD_LIBRARY_PATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/lib;
+               export PYTHONPATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/lib/python2.7/dist-packages;
+               cd software/model/muscles/parameter_estimation;
+               nice -n 10 python parest.py parest_$i.pickle;
+               exec bash
+               "
+    else
+        sleep 300
+    fi
 done
