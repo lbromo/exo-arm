f73edea8a1582c5e86d5319b33f4c7a1f574daed
Lasse Bromose <lbromo@protonmail.ch>
2017-03-25 14:59:56 +0100

    stuff

--- run_parest.sh
+++ run_parest.sh
@@ -8,18 +8,18 @@ fi
 
 for i in $(seq 1 $RUNS)
 do
-    if [ "$(ps -ef | grep -v grep | grep scrape_data.php | wc -l)" -lt  "12" ]
+    if [ "$(ps -ef | grep -v grep | grep parest.py | wc -l)" -lt  "36" ]
     then
-        # screen -dmS "parest_$i" \
-        #        bash -c         \
-        #        "
-        #        source venv/bin/activate;
-        #        export LD_LIBRARY_PATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/lib;
-        #        export PYTHONPATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/lib/python2.7/dist-packages;
-        #        cd software/model/muscles/parameter_estimation;
-        #        nice -n 10 python parest.py parest_$i.pickle;
-        #        exec bash
-        #        "
+        screen -dmS "parest_$i" \
+               bash -c         \
+               "
+               source venv/bin/activate;
+               export LD_LIBRARY_PATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/local/lib;
+               export PYTHONPATH=/afs/ies.auc.dk/group/17gr1035/no_backup/usr/lib/python2.7/dist-packages;
+               cd software/model/muscles/parameter_estimation;
+               nice -n 10 python parest.py parest_$i.pickle;
+               exec bash
+               "
     else
         sleep 300
     fi
