c9e6110d86874cc317a53971dbbdf108b6c03460
Lasse Bromose <lbromo@protonmail.ch>
2017-03-27 17:02:37 +0200

    added stuff

--- software/model/muscles/parameter_estimation/plot_parest.py
+++ software/model/muscles/parameter_estimation/plot_parest.py
@@ -1,6 +1,17 @@
 import parest
 import numpy as np
+from scipy import signal
 import matplotlib.pyplot as plt
+
+import pickle
+import glob, os
+
+import sys
+sys.path.append("../")
+import muscle_utils
+
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
 
 torque = []
 angles = []
@@ -39,8 +50,100 @@ np.savetxt('imu1.dat', imu1_vstack, delimiter=',', header='ACC1, ACC2, ACC3', co
 np.savetxt('torque.dat', torque, header='TORQUE', comments='')
 np.savetxt('angles.dat', angles, header='ANGLE', comments='')
 
+FILE_MSE = {}
+
+for file in glob.glob("/afs/ies.auc.dk/group/17gr1035/Private/exo-arm/software/model/muscles/parameter_estimation/*.pickle"):
+    with open(file, 'rb') as tmp:
+        values = pickle.load(tmp)
+        FILE_MSE[file] = values['MSE']
+
+best_parest = min(FILE_MSE, key=FILE_MSE.get)
+
+pars = {}
+with open(best_parest, 'rb') as tmp:
+    pars = pickle.load(tmp)
+
+pp.pprint(pars)
+
+triceps_pars = [
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['C1'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['C2'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['A'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['d'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['max_length'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['optimal_fiber_length'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['tensor_slack_length'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['max_force'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['alpha'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['Spe'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['Sse'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['phi_m'],
+    pars[muscle_utils.MUSCLE_NAME.TRICEPS_BRACHII]['phi_v'],
+]
+
+biceps_pars = [
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['C1'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['C2'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['A'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['d'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['max_length'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['optimal_fiber_length'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['tensor_slack_length'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['max_force'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['alpha'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['Spe'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['Sse'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['phi_m'],
+    pars[muscle_utils.MUSCLE_NAME.BICEPS_BRACHII]['phi_v'],
+]
+
+brachialis_pars = [
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['C1'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['C2'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['A'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['d'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['max_length'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['optimal_fiber_length'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['tensor_slack_length'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['max_force'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['alpha'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['Spe'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['Sse'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['phi_m'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIALIS]['phi_v'],
+]
+
+brachioradialis_pars = [
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['C1'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['C2'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['A'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['d'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['max_length'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['optimal_fiber_length'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['tensor_slack_length'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['max_force'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['alpha'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['Spe'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['Sse'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['phi_m'],
+    pars[muscle_utils.MUSCLE_NAME.BRACHIORADIALIS]['phi_v'],
+]
+
+
+params = triceps_pars + biceps_pars + brachialis_pars + brachioradialis_pars
+
+print(len(params))
+
+estimated = parest.FlexProblem()
+muscles = estimated.set_up_muscles(params)
+tau, act, _ = estimated.simulate(muscles)
+
+b, a = signal.butter(4, 1/(0.5*100), 'lowpass')
+tau = signal.lfilter(b, a, tau)
+
 plt.subplot(2, 1, 1)
-plt.plot(torque)
+plt.plot(torque, linewidth=0.5)
+plt.plot(tau, '--',linewidth=0.5)
 plt.subplot(2, 1, 2)
 plt.plot(angles)
 
@@ -65,3 +168,5 @@ for i in range(1,4):
     plt.plot(imu1[:,i])
 
 plt.show()
+
+
