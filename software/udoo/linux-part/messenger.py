f0d97353108da44f2847527b5104132b93571c83
Lasse Bromose <lbromo@protonmail.ch>
2017-04-28 10:02:02 +0200

    can now do 100 Hz

--- software/udoo/linux-part/messenger.py
+++ software/udoo/linux-part/messenger.py
@@ -1,5 +1,4 @@
-import threading
-import time
+import time, io, threading
 import serial
 import numpy as np
 
@@ -13,105 +12,97 @@ END_CHAR = b'E'
 
 SER_LOCK = threading.Lock()
 
+LOGGING = True
+
+def intTo3Bytes(intvar):
+    return str.encode(str(intvar).zfill(3))
+
 def log1msg(ser, logfile):
-    if ser.isOpen():
-        initmsg = ser.readline()
-        if not len(initmsg):
-            return
+    msg = ser.read(ser.in_waiting)
 
-        # print(initmsg)
-        initmsg = initmsg.decode()
+    if msg:
+        msg = msg.replace(b'$,', b'').replace(b'\r', b'')
+        logfile.write(msg)
 
-        if initmsg[0] == START_CHAR:
-            msg = initmsg[2:]
-            # print(msg)
-            data_w_units = msg.strip().split(',')
-            # NICE PRINTING FUNCTION
-            # print(','.join(str(x) for x in data_w_units))
-            logfile.write(','.join(str(x) for x in data_w_units) + '\n')
+    return len(msg)
 
-def tUpdateRef(ser):
-    while True:
-        try:
-            ref_shoulder, ref_elbow = [int(var) for var in raw_input("Enter new reference: ").split()]
-        except:
-            print("Error getting new ref. Please input as <shoulder_ref elbow_ref>")
-        else:
-            ref = np.array([
-                np.radians(ref_shoulder),
-                np.radians(ref_elbow),
-                0,
-                0
-            ])
-            with SER_LOCK:
-                if ser.isOpen():
-                    print('Going to: ('+ str(ref[0]) +',', str(ref[1]) + ')')
-                    ref_msg = REF_CHAR + (intTo3Bytes(int(ref[0]*100))) + b',' + (intTo3Bytes(int(ref[1]*100))) + b',' + (intTo3Bytes(int(ref[2]*100))) + b',' + (intTo3Bytes(int(ref[3]*100))) + b',' + END_CHAR
-                    ser.write(ref_msg)
+def updateRef(ser):
+    try:
+        ref_shoulder, ref_elbow = [int(var) for var in raw_input("Enter new reference: ").split()]
+    except KeyboardInterrupt:
+        raise
+    except:
+        print("Error getting new ref. Please input as <shoulder_ref elbow_ref>")
+    else:
+        ref = np.array([
+            np.radians(ref_shoulder),
+            np.radians(ref_elbow),
+            0,
+            0
+        ])
+        print('Going to: ('+ str(ref[0]) +',', str(ref[1]) + ')')
+        ref_msg = REF_CHAR + (intTo3Bytes(int(ref[0]*100))) + b',' + (intTo3Bytes(int(ref[1]*100))) + b',' + (intTo3Bytes(int(ref[2]*100))) + b',' + (intTo3Bytes(int(ref[3]*100))) + b',' + END_CHAR
+        with SER_LOCK:
+            ser.write(ref_msg)
 
 def tLogger(ser, f):
-    while True:
+    while LOGGING:
         t_start = time.time()
         with SER_LOCK:
             ser.write(READY)
         log1msg(ser,f)
-        t_sleep = time.time() - t_start;
+        time.sleep(0.01)
 
-        if 0 < t_sleep:
-            time.sleep(t_sleep)
-    
+    #ser.reset_output_buffer()
+    #ser.reset_input_buffer()
 
-
-
-def manual_input_mode(ser, logfile):
-    t = threading.Thread(target=tUpdateRef, args=(ser,))
-    t.daemon = True
-    t.start()
-
-    f = open(logfile, 'w')
+def manual_input_mode(ser, logger_h, log_file_h):
+    global LOGGING
 
     while True:
         try:
-            with SER_LOCK:
-                ser.write(READY)
-                log1msg(ser, f)
+            updateRef(ser)
         except:
-            time.sleep(0.01)
+            LOGGING = False
+            logger_h.join()
+            log_file_h.close()
             ser.write(b'S')
-            time.sleep(0.01)
-            f.close()
-            exit()
+            ser.close()
+            break
         else:
             time.sleep(0.01)
 
-def csv_input_mode(ser, logfile, datafile):
-    f = open(logfile, 'w')
+def csv_input_mode(ser, logger_h, log_file_h, datafile):
+    global LOGGING
     data = np.genfromtxt(datafile,delimiter=',')
 
-    # logger = threading.Thread(target=tLogger, args=(ser, f))
-    # logger.daemon = True
-    # logger.start()
-    
-    # print(data.shape)
-    print(time.time())
-    #
-    for idx in range(1,len(data),4):
+    start_time = time.time()
+
+    for idx in range(1,len(data),1):
         begin = time.time()
-        
-        ref_msg = REF_CHAR + intTo3Bytes(int(data[idx,0]*100)) + b',' + intTo3Bytes(int(data[idx,0]*100)) + b',' + intTo3Bytes(int(data[idx,1]*100)) + b',' + intTo3Bytes(int(data[idx,1]*100)) + b',' + END_CHAR
-        # with SER_LOCK:
-        ser.write(ref_msg)
-        ser.write(READY)
-        log1msg(ser, f)
-    
-        sleep_time = 0.04 - (time.time() - begin)
+
+        ref_msg = REF_CHAR + \
+                  intTo3Bytes(int(data[idx,0]*100)) + b',' + \
+                  intTo3Bytes(int(data[idx,0]*100)) + b',' + \
+                  intTo3Bytes(int(data[idx,1]*100)) + b',' + \
+                  intTo3Bytes(int(data[idx,1]*100)) + b',' + \
+                  END_CHAR
+
+        with SER_LOCK:
+            ser.write(ref_msg)
+
+        sleep_time = 0.01 - (time.time() - begin)
         if sleep_time > 0:
             time.sleep(sleep_time)
-    #
-    print(time.time())
 
-def intTo3Bytes(intvar):
-    return str.encode(str(intvar).zfill(3))
+    print(time.time() - start_time)
+    LOGGING = False
+    logger_h.join()
+    log_file_h.close()
+
+    ser.write(b'S')
+    ser.close()
+
 
 if __name__ == "__main__":
     import sys
@@ -119,19 +110,24 @@ if __name__ == "__main__":
         LOG_FILE = sys.argv[1]
         SER_PORT = sys.argv[2]
     else:
-        LOG_FILE = 'default.log'
+        LOG_FILE = "default.log"
         SER_PORT = "/dev/ttyUSB0"
 
     ser = serial.Serial(SER_PORT, BAUD, timeout=2)
+    bw = io.BufferedWriter(io.FileIO(LOG_FILE, 'wb'))
+
+    t = threading.Thread(target=tLogger, args=(ser, bw))
+    t.daemon = False
+    t.start()
 
     if not ser.isOpen():
         ser.open()
         print("Serial Open")
 
     if len(sys.argv) == 4:
-        csv_input_mode(ser, LOG_FILE, sys.argv[3])
+        csv_input_mode(ser, t, bw, sys.argv[3])
     else:
-        manual_input_mode(ser, LOG_FILE)
+        manual_input_mode(ser, t, bw)
 
 
 
