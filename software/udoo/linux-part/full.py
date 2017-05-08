8038457361d93d6a69eaf2d9ad7eb22aab834bb9
Lasse Bromose <lbromo@protonmail.ch>
2017-05-08 14:35:52 +0200

    uses real angles ala mortens idea

--- software/udoo/linux-part/full.py
+++ software/udoo/linux-part/full.py
@@ -91,15 +91,15 @@ def update(emg_meas):
 
     tmp = tau * B
 
-    ref[0] = ref[0] + ref[2] * 0.01
-    ref[1] = ref[1] + ref[3] * 0.01
+    ref[0] = angles[0] + ref[2] * 0.01
+    ref[1] = angles[1] + ref[3] * 0.01
     ref[2] = tmp[0]
     ref[3] = tmp[1]
 
-    if ref[0] > np.pi/2:
-        ref[0] = np.pi/2
-    if ref[1] > np.pi/2:
-        ref[1] = np.pi/2
+    # if ref[0] > np.pi/2:
+    #     ref[0] = np.pi/2
+    # if ref[1] > np.pi/2:
+    #     ref[1] = np.pi/2
 
     ref_msg = REF_CHAR + (intTo3Bytes(int(ref[0]*100))) + b',' + (intTo3Bytes(int(ref[1]*100))) + b',' + (intTo3Bytes(int(ref[2]*100))) + b',' + (intTo3Bytes(int(ref[3]*100))) + b',' + END_CHAR
 
