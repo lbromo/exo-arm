9287dab73ae45c04d90e5cd9a4d9778dffad36cb
DisasterMaster <mortenrechter@hotmail.com>
2017-05-19 08:48:01 +0200

    Made new n vector without gravity compensation

--- model/system_matrices/n.m
+++ model/system_matrices/n.m
@@ -4,7 +4,7 @@ function [n] = n(x,params, viewflag)
   	G=g_matrix(x(1:2), params.g, params.l1, params.l2, params.m1, params.m2, params.a1 ,params.a2);
   	F=f_matrix(x(3:4), params.vm, params.cm, params.N, params.sigmoidpar);
 
-  	n = G + V + F;
+  	n = V + F;
 
   	if exist('viewflag') && viewflag == 1
   		disp('V:')
